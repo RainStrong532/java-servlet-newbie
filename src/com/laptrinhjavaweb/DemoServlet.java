f74e45198e5ce29061ae488604cdd80277ab8649
Mạnh Vũ Văn <vuvanmanhttvtvp@gmail.com>
2021-03-22 04:13:04 +0700

    add hello world

---
+++ src/com/laptrinhjavaweb/DemoServlet.java
@@ -0,0 +1,22 @@
+package com.laptrinhjavaweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DemoServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		
+		PrintWriter printWriter = resp.getWriter();
+		
+		printWriter.println("<h1>Hello world</>");
+		
+		printWriter.close();
+	}
+}
